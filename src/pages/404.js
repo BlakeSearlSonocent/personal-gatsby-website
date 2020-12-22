2a4a4b2d707aab66a86a5e067cebd57074e10d3e
Blake Searl <blake.searl@sonocent.com>
2020-12-22 22:23:39 +0000

    First few page outlines

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const error = () => {
+  return (
+    <div>
+      <h1>This page don't exist :(</h1>
+    </div>
+  )
+}
+
+export default error
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {Link} from "gatsby"
+
+export default () => (
+  <div>
+    Blake's website
+    <div>
+      <Link to="/blog">Blog page</Link>
+    </div>
+  </div>
+)
